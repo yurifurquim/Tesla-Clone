3239be8d1f66a93ec3251d1abf6fdd57b5e08770
yurifurquim <yurifurquimdev@gmail.com>
2022-10-17 14:13:19 -0300

    Update

---
+++ tesla-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+import Section from "./components/Section"
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
